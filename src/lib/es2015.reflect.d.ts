e70e837c52a31548f2655ce7243f00a8f9602d08
Jake Verbaten <raynos2@gmail.com>
2020-05-05 17:31:15 +0200

    Change `Reflect.get()` to return `unknown`

--- src/lib/es2015.reflect.d.ts
+++ src/lib/es2015.reflect.d.ts
@@ -3,7 +3,7 @@ declare namespace Reflect {
     function construct(target: Function, argumentsList: ArrayLike<any>, newTarget?: any): any;
     function defineProperty(target: object, propertyKey: PropertyKey, attributes: PropertyDescriptor): boolean;
     function deleteProperty(target: object, propertyKey: PropertyKey): boolean;
-    function get(target: object, propertyKey: PropertyKey, receiver?: any): any;
+    function get(target: object, propertyKey: PropertyKey, receiver?: any): unknown;
     function getOwnPropertyDescriptor(target: object, propertyKey: PropertyKey): PropertyDescriptor | undefined;
     function getPrototypeOf(target: object): object;
     function has(target: object, propertyKey: PropertyKey): boolean;
